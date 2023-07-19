c25c2a18e55d3d3fdb93ac49d21592e257dfd2fa
Lukas Stateczny <statecznylukas@gmail.com>
2023-07-19 13:37:06 +0800

    Petal documents contract added

---
+++ petal_documents/target/debug/deps/petal_documents-171542c6b96fddde.d
@@ -0,0 +1,5 @@
+/Users/lukasstateczny/Desktop/Mojoflower/petal-contracts/petal_documents/target/debug/deps/petal_documents-171542c6b96fddde.rmeta: src/lib.rs
+
+/Users/lukasstateczny/Desktop/Mojoflower/petal-contracts/petal_documents/target/debug/deps/petal_documents-171542c6b96fddde.d: src/lib.rs
+
+src/lib.rs:
