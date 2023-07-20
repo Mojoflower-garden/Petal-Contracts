43857f28464e740a9262d37955dcbd00de115332
Lukas Stateczny <statecznylukas@gmail.com>
2023-07-20 08:51:41 +0800

    Deleted disfunctional contract

--- petal_documents/target/debug/deps/petal_documents-30ec6009ed976ffb.d
+++
@@ -1,5 +0,0 @@
-/Users/lukasstateczny/Desktop/Mojoflower/petal-contracts/petal_documents/target/debug/deps/petal_documents-30ec6009ed976ffb.rmeta: src/lib.rs
-
-/Users/lukasstateczny/Desktop/Mojoflower/petal-contracts/petal_documents/target/debug/deps/petal_documents-30ec6009ed976ffb.d: src/lib.rs
-
-src/lib.rs:
